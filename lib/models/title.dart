1372b6825ed4b5b714d04b5a9fbff676b898175b
Cavid Quluzadə <cqthor@gmail.com>
2022-07-06 18:55:53 +0400

    just commit

--- lib/models/title.dart
+++ lib/models/title.dart
@@ -12,6 +12,11 @@ class Title {
   String? plot;
   String? directors;
   List<ActorList>? actorList;
+  List<Similars>? similars;
+  List<GenreList>? genreList;
+  List<CompanyList>? companyList;
+  List<CountryList>? countryList;
+  List<LanguageList>? languageList;
   Title({
     this.id,
     this.title,
@@ -21,6 +26,11 @@ class Title {
     this.actorList,
     this.runtimeMins,
     this.directors,
+    this.similars,
+    this.genreList,
+    this.companyList,
+    this.countryList,
+    this.languageList,
   });
 
   factory Title.fromJson(Map<String, dynamic> json) => _$TitleFromJson(json);
@@ -34,4 +44,61 @@ class ActorList {
   factory ActorList.fromJson(Map<String, dynamic> json) =>
       _$ActorListFromJson(json);
   Map<String, dynamic> toJson() => _$ActorListToJson(this);
+}
+
+@JsonSerializable()
+class GenreList {
+  String? key;
+  String? value;
+
+  GenreList({this.key, this.value});
+
+  factory GenreList.fromJson(Map<String, dynamic> json) =>
+      _$GenreListFromJson(json);
+  Map<String, dynamic> toJson() => _$GenreListToJson(this);
+}
+
+@JsonSerializable()
+class Similars {
+  String? id;
+  String? title;
+  String? image;
+  String? imDbRating;
+
+  Similars({this.id, this.title, this.image, this.imDbRating});
+  factory Similars.fromJson(Map<String, dynamic> json) =>
+      _$SimilarsFromJson(json);
+  Map<String, dynamic> toJson() => _$SimilarsToJson(this);
+}
+
+@JsonSerializable()
+class CompanyList {
+  String? id;
+  String? name;
+
+  CompanyList({this.id, this.name});
+
+  factory CompanyList.fromJson(Map<String, dynamic> json) =>
+      _$CompanyListFromJson(json);
+  Map<String, dynamic> toJson() => _$CompanyListToJson(this);
+}
+
+@JsonSerializable()
+class CountryList {
+  String? key;
+  String? value;
+  CountryList({this.key, this.value});
+  factory CountryList.fromJson(Map<String, dynamic> json) =>
+      _$CountryListFromJson(json);
+  Map<String, dynamic> toJson() => _$CountryListToJson(this);
+}
+
+@JsonSerializable()
+class LanguageList {
+  String? key;
+  String? value;
+  LanguageList({this.key, this.value});
+  factory LanguageList.fromJson(Map<String, dynamic> json) =>
+      _$LanguageListFromJson(json);
+  Map<String, dynamic> toJson() => _$LanguageListToJson(this);
 }
--- lib/models/title.g.dart
+++ lib/models/title.g.dart
@@ -17,6 +17,21 @@ Title _$TitleFromJson(Map<String, dynamic> json) => Title(
           .toList(),
       runtimeMins: json['runtimeMins'] as String?,
       directors: json['directors'] as String?,
+      similars: (json['similars'] as List<dynamic>?)
+          ?.map((e) => Similars.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      genreList: (json['genreList'] as List<dynamic>?)
+          ?.map((e) => GenreList.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      companyList: (json['companyList'] as List<dynamic>?)
+          ?.map((e) => CompanyList.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      countryList: (json['countryList'] as List<dynamic>?)
+          ?.map((e) => CountryList.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      languageList: (json['languageList'] as List<dynamic>?)
+          ?.map((e) => LanguageList.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
 
 Map<String, dynamic> _$TitleToJson(Title instance) => <String, dynamic>{
@@ -28,13 +43,18 @@ Map<String, dynamic> _$TitleToJson(Title instance) => <String, dynamic>{
       'plot': instance.plot,
       'directors': instance.directors,
       'actorList': instance.actorList,
+      'similars': instance.similars,
+      'genreList': instance.genreList,
+      'companyList': instance.companyList,
+      'countryList': instance.countryList,
+      'languageList': instance.languageList,
     };
 
 ActorList _$ActorListFromJson(Map<String, dynamic> json) => ActorList(
-      id: json['id'] as String,
-      name: json['name'] as String,
-      image: json['image'] as String,
-      asCharacter: json['asCharacter'] as String,
+      id: json['id'] as String?,
+      name: json['name'] as String?,
+      image: json['image'] as String?,
+      asCharacter: json['asCharacter'] as String?,
     );
 
 Map<String, dynamic> _$ActorListToJson(ActorList instance) => <String, dynamic>{
@@ -42,4 +62,61 @@ Map<String, dynamic> _$ActorListToJson(ActorList instance) => <String, dynamic>{
       'name': instance.name,
       'image': instance.image,
       'asCharacter': instance.asCharacter,
+    };
+
+GenreList _$GenreListFromJson(Map<String, dynamic> json) => GenreList(
+      key: json['key'] as String?,
+      value: json['value'] as String?,
+    );
+
+Map<String, dynamic> _$GenreListToJson(GenreList instance) => <String, dynamic>{
+      'key': instance.key,
+      'value': instance.value,
+    };
+
+Similars _$SimilarsFromJson(Map<String, dynamic> json) => Similars(
+      id: json['id'] as String?,
+      title: json['title'] as String?,
+      image: json['image'] as String?,
+      imDbRating: json['imDbRating'] as String?,
+    );
+
+Map<String, dynamic> _$SimilarsToJson(Similars instance) => <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'image': instance.image,
+      'imDbRating': instance.imDbRating,
+    };
+
+CompanyList _$CompanyListFromJson(Map<String, dynamic> json) => CompanyList(
+      id: json['id'] as String?,
+      name: json['name'] as String?,
+    );
+
+Map<String, dynamic> _$CompanyListToJson(CompanyList instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+    };
+
+CountryList _$CountryListFromJson(Map<String, dynamic> json) => CountryList(
+      key: json['key'] as String?,
+      value: json['value'] as String?,
+    );
+
+Map<String, dynamic> _$CountryListToJson(CountryList instance) =>
+    <String, dynamic>{
+      'key': instance.key,
+      'value': instance.value,
+    };
+
+LanguageList _$LanguageListFromJson(Map<String, dynamic> json) => LanguageList(
+      key: json['key'] as String?,
+      value: json['value'] as String?,
+    );
+
+Map<String, dynamic> _$LanguageListToJson(LanguageList instance) =>
+    <String, dynamic>{
+      'key': instance.key,
+      'value': instance.value,
     };
